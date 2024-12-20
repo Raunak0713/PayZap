01f6d6e563ae51460c90a8eb5116f8083a9ae44e
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-12-20 19:05:59 +0530

    #4 Implement Authentication

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,14 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { signIn } from '../utils/auth'
+import { auth, signIn } from '../utils/auth'
+import { redirect } from 'next/navigation'
 
-const LoginPage = () => {
+const LoginPage = async  () => {
+  const session = await auth()
+  if(session?.user){
+    redirect("/dashboard")
+  }
   return (
     <>
       <div className='flex h-screen w-full items-center justify-center px-4'>
--- components/submitButtons.tsx
+++ components/submitButtons.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client"
+import { useFormStatus } from 'react-dom'
+import { Button } from './ui/button'
+import { Loader2 } from 'lucide-react'
+
 
 const SubmitButtons = () => {
+  const { pending } = useFormStatus()
   return (
-    <div>SubmitButtons</div>
+    <div>
+      {pending ? (
+        <div>
+          <Button disabled className='w-full'>
+            <Loader2 className='size-4 mr-2 animate-spin'/>
+            Please Wait....
+          </Button>
+        </div>
+        
+      ) : (
+        <Button className='w-full' type='submit'>
+          Submit
+        </Button>
+      )}
+    </div>
   )
 }
 
-export default SubmitButtons
+export default SubmitButtons 
